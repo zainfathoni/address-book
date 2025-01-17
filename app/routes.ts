8c2190b08709c0ffe9927e2745414e5feba61b15
Zain Fathoni <me@zainf.dev>
2025-01-18 06:33:53 +0800

    feat: Index Routes
    https://reactrouter.com/tutorials/address-book#index-routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import type { RouteConfig } from "@react-router/dev/routes";
-import { route } from "@react-router/dev/routes";
+import { index, route } from "@react-router/dev/routes";
 
 export default [
+  index("routes/home.tsx"),
   route("contacts/:contactId", "routes/contact.tsx"),
 ] satisfies RouteConfig;
