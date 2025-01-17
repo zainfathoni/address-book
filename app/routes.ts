51ffcfacc81c1129322b198f1b0b62127d12f2f5
Zain Fathoni <me@zainf.dev>
2025-01-18 06:38:24 +0800

    feat: Adding an About Route
    https://reactrouter.com/tutorials/address-book#adding-an-about-route

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ import { index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("contacts/:contactId", "routes/contact.tsx"),
+  route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
