0207cdde869b3014ed782c46ec677006fcb6f0a9
Zain Fathoni <me@zainf.dev>
2025-01-18 06:23:34 +0800

    feat: Loading Data
    https://reactrouter.com/tutorials/address-book#loading-data

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,15 @@ import type { Route } from "./+types/root";
 
 import appStylesHref from "./app.css?url";
 
-export default function App() {
+import { getContacts } from "./data";
+
+export async function clientLoader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export default function App({ loaderData }) {
+  const { contacts } = loaderData;
   return (
     <>
       <div id="sidebar">
@@ -31,14 +39,26 @@ export default function App() {
           </Form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`/contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}
+                    {contact.favorite ? <span>★</span> : null}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No contacts</p>
+          )}
         </nav>
       </div>
       <div id="detail">
