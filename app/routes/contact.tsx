0462ad579ea0ba1c8bd228642298fc75efbe9244
Zain Fathoni <me@zainf.dev>
2025-01-18 13:37:14 +0800

    refactor: Attempt to render Contact as React Server Component

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -8,9 +8,67 @@ import type { Route } from "./+types/contact";
 
 export async function loader({ params }: Route.LoaderArgs) {
   const contact = await getContact(params.contactId);
+
+  const renderedContact = contact ? (
+    <div id="contact">
+      <title>{`${contact.first} ${contact.last} - React Router Contacts`}</title>
+      <div>
+        <img
+          alt={`${contact.first} ${contact.last} avatar`}
+          key={contact.avatar}
+          src={contact.avatar}
+        />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter ? (
+          <p>
+            <a href={`https://twitter.com/${contact.twitter}`}>
+              {contact.twitter}
+            </a>
+          </p>
+        ) : null}
+
+        {contact.notes ? <p>{contact.notes}</p> : null}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+
+          <Form
+            action="destroy"
+            method="post"
+            onSubmit={(event) => {
+              const response = confirm(
+                "Please confirm you want to delete this record.",
+              );
+              if (!response) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  ) : null;
+
   // TODO: use github username instead of twitter
   const githubUser = getGithubUser(contact?.twitter?.substring(1));
-  return { contact, githubUser };
+  return { contact, renderedContact, githubUser };
 }
 
 export async function action({ request, params }: Route.ActionArgs) {
@@ -21,7 +79,7 @@ export async function action({ request, params }: Route.ActionArgs) {
 }
 
 export default function Contact({ loaderData }: Route.ComponentProps) {
-  const { contact, githubUser } = loaderData;
+  const { contact, renderedContact, githubUser } = loaderData;
 
   if (!contact) {
     throw new Response("Not Found", { status: 404 });
@@ -29,60 +87,7 @@ export default function Contact({ loaderData }: Route.ComponentProps) {
 
   return (
     <main id="contact-page">
-      <div id="contact">
-        <title>{`${contact.first} ${contact.last} - React Router Contacts`}</title>
-        <div>
-          <img
-            alt={`${contact.first} ${contact.last} avatar`}
-            key={contact.avatar}
-            src={contact.avatar}
-          />
-        </div>
-
-        <div>
-          <h1>
-            {contact.first || contact.last ? (
-              <>
-                {contact.first} {contact.last}
-              </>
-            ) : (
-              <i>No Name</i>
-            )}
-            <Favorite contact={contact} />
-          </h1>
-
-          {contact.twitter ? (
-            <p>
-              <a href={`https://twitter.com/${contact.twitter}`}>
-                {contact.twitter}
-              </a>
-            </p>
-          ) : null}
-
-          {contact.notes ? <p>{contact.notes}</p> : null}
-
-          <div>
-            <Form action="edit">
-              <button type="submit">Edit</button>
-            </Form>
-
-            <Form
-              action="destroy"
-              method="post"
-              onSubmit={(event) => {
-                const response = confirm(
-                  "Please confirm you want to delete this record.",
-                );
-                if (!response) {
-                  event.preventDefault();
-                }
-              }}
-            >
-              <button type="submit">Delete</button>
-            </Form>
-          </div>
-        </div>
-      </div>
+      {renderedContact}
       <div id="github">
         <Suspense
           fallback={
